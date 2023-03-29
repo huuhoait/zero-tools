0fab7c4457ab395e374cf4603b4af287ea73947d
Hoa Le <huuhoa.it.vn@gmail.com>
2023-03-29 20:58:54 +0700

    add fields

--- orm/ent/mixins/base.go
+++ orm/ent/mixins/base.go
@@ -33,9 +33,13 @@ type BaseIDMixin struct {
 func (BaseIDMixin) Fields() []ent.Field {
 	return []ent.Field{
 		field.Uint64("id"),
+		field.String("created_by").
+			Optional(),
 		field.Time("created_at").
 			Immutable().
 			Default(time.Now),
+		field.String("updated_by").
+			Optional(),
 		field.Time("updated_at").
 			Default(time.Now).
 			UpdateDefault(time.Now),
