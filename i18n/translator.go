0d784a0ca283deb6fd89988ad8127cd17523d3b7
Hoa Le <huuhoa.it.vn@gmail.com>
2023-03-31 16:48:20 +0700

    a

--- i18n/translator.go
+++ i18n/translator.go
@@ -22,14 +22,16 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/huuhoait/zero-tools/utils/errcode"
+	"github.com/huuhoait/zero-tools/utils/parse"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"github.com/zeromicro/go-zero/core/errorx"
 	"github.com/zeromicro/go-zero/core/logx"
 	"golang.org/x/text/language"
 	"google.golang.org/grpc/status"
 
-	"github.com/huuhoait/zero-tools/i18n/errorx"
-	"github.com/huuhoait/zero-tools/utils/errcode"
-	"github.com/huuhoait/zero-tools/utils/parse"
+	"github.com/suyuan32/simple-admin-common/utils/errcode"
+	"github.com/suyuan32/simple-admin-common/utils/parse"
 )
 
 //go:embed locale/*.json
